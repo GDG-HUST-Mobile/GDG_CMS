8143274c8d865850a05c4855f7f950b182eda7fc
Huynguyen736 <nguyenhuy07032006@gmail.com>
2025-07-26 11:47:10 +0700

    chore: login UI responsive for mobile

--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -1,11 +1,9 @@
+// lib/features/login/ui/login_screen.dart
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:gdgocms/features/login/ui/components/textfield.dart';
-import 'package:gdgocms/features/login/ui/forgotpass_popup.dart';
-import 'package:gdgocms/features/login/ui/signup_popup.dart';
 import 'package:gdgocms/core/theme/app_colors.dart';
-import 'package:gdgocms/core/theme/app_fonts.dart';
-import 'package:gdgocms/core/theme/app_theme.dart';
-import 'package:gdgocms/core/theme/app_images.dart';
+import 'package:gdgocms/features/login/ui/factories/login_layout_factory.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -20,195 +18,46 @@ class _LoginScreenState extends State<LoginScreen> {
   bool checkAccount = true;
   bool _isPasswordObscured = true;
 
+  void _togglePasswordVisibility() {
+    setState(() {
+      _isPasswordObscured = !_isPasswordObscured;
+    });
+  }
+
+  void _handleSignIn() {
+    setState(() {
+      checkAccount = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final Size size = MediaQuery.of(context).size;
-    final double screenHeight = size.height;
-    final double screenWidth = size.width;
-
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: AppColors.backgroundLight,
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Expanded(
-              child: SingleChildScrollView(
-                padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
-                child: Column(
-                  children: [
-                    SizedBox(height: screenHeight * 0.03),
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            const double minLayoutWidth = 370.0;
 
-                    SizedBox(
-                      width: screenWidth * 0.9,
-                      child: Image.asset(AppImages.logoGdg, fit: BoxFit.contain),
-                    ),
+            final double layoutWidth = max(minLayoutWidth, constraints.maxWidth);
 
-                    SizedBox(height: screenHeight * 0.03),
-
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        // login text
-                        Text("Sign in", style: AppFonts.displayLargeText()),
-
-                        SizedBox(height: screenHeight * 0.03),
-
-                        // username field
-                        Text("ID", style: AppFonts.titleLargeText()),
-                        SizedBox(height: screenHeight * 0.01),
-                        MyTextField(
-                          controller: usernameController,
-                          hintText: "Your ID",
-                          obscureText: false,
-                        ),
-
-                        SizedBox(height: screenHeight * 0.03),
-
-                        // password field
-                        Text("Password", style: AppFonts.titleLargeText()),
-                        SizedBox(height: screenHeight * 0.01),
-                        MyTextField(
-                          controller: passwordController,
-                          hintText: "Enter your password",
-                          obscureText: _isPasswordObscured,
-                          suffixIcon: IconButton(
-                            icon: Icon(
-                              _isPasswordObscured ? Icons.visibility : Icons.visibility_off,
-                              color: AppColors.textSecondary,
-                            ),
-                            onPressed: () {
-                              setState(() {
-                                _isPasswordObscured = !_isPasswordObscured;
-                              });
-                            },
-                          ),
-                        ),
-
-                        SizedBox(height: screenHeight * 0.01),
-
-                        // forgot password
-                        Align(
-                          alignment: Alignment.centerRight,
-                          child: GestureDetector(
-                            onTap: () {
-                              showForgotPasswordDialog(context);
-                            },
-                            child: Text(
-                              "Forgot Password?",
-                              style: AppFonts.titleLargeText(
-                                color: AppColors.textLink,
-                              ),
-                            ),
-                          ),
-                        ),
-
-                        SizedBox(height: screenHeight * 0.02),
-                      ],
-                    ),
-
-                    if (!checkAccount)
-                      Padding(
-                        padding: EdgeInsets.only(bottom: screenHeight * 0.015),
-                        child: Text(
-                          "Oops! Incorrect password. Try again!",
-                          textAlign: TextAlign.center,
-                          style: AppFonts.errorMediumText(
-                            color: AppColors.textError,
-                          )
-                        ),
-                      ),
-
-                    // signin button
-                    SizedBox(
-                      width: screenWidth * 0.75,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          setState(() {
-                            checkAccount = false;
-                          });
-                        },
-                        style: AppButtonStyles.primary.copyWith(
-                          padding: WidgetStateProperty.all(
-                            EdgeInsets.symmetric(vertical: screenHeight * 0.02),
-                          ),
-                        ),
-                        child: Text("Sign In", style: AppFonts.buttonLarge()),
-                      ),
-                    ),
-
-                    SizedBox(height: screenHeight * 0.04),
-
-                    // others signin
-                    Row(
-                      children: [
-                        Expanded(
-                          child: Divider(thickness: 0.5, color: Colors.grey[400]),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
-                          child: Text(
-                            "Or continue with",
-                            style: AppFonts.headlineSmallText(
-                              color: AppColors.textSecondary,
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          child: Divider(thickness: 0.5, color: Colors.grey[400]),
-                        ),
-                      ],
-                    ),
-
-                    SizedBox(height: screenHeight * 0.03),
-
-                    IconButton(
-                      onPressed: () {},
-                      icon: ClipOval(
-                        child: Image.asset(
-                          AppImages.googleIcon,
-                          width: screenWidth * 0.15,
-                          height: screenWidth * 0.15,
-                          fit: BoxFit.cover,),
-                      ),
-                      padding: EdgeInsets.zero,
-                      constraints: const BoxConstraints(),
-                    ),
-
-                    SizedBox(height: screenHeight * 0.03),
-
-                    // register
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Not a member?",
-                          style: AppFonts.headlineSmallText(
-                            color: AppColors.textSecondary,
-                          ),
-                        ),
-                        const SizedBox(width: 4),
-                        GestureDetector(
-                          onTap: () {
-                            showSignUpDialog(context);
-                          },
-                          child: Text(
-                            "Create an account",
-                            style: AppFonts.headlineSmallText(
-                              color: AppColors.textLink,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: screenHeight * 0.02),
-                  ],
+            return SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: SizedBox(
+                width: layoutWidth,
+                child: LoginLayoutFactory.build(
+                  context: context,
+                  usernameController: usernameController,
+                  passwordController: passwordController,
+                  isPasswordObscured: _isPasswordObscured,
+                  checkAccount: checkAccount,
+                  onPasswordToggle: _togglePasswordVisibility,
+                  onSignIn: _handleSignIn,
                 ),
               ),
-            ),
-            Image.asset(AppImages.loginFooter),
-          ],
+            );
+          },
         ),
       ),
     );
